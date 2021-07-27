fe708091e03f968024f41dd68c0dd8102a0605b5
glucrypto <glucrypto@protonmail.com>
2021-07-27 11:12:01 -0400

    init commit

---
+++ pool_test.js
@@ -0,0 +1,27 @@
+export const fantomPools = [
+  {
+    id: 'spirit-spirit-ftm',
+    logo: 'single-assets/BIFI.png',
+    name: 'SPIRIT-FTM LP',
+    token: 'FTM-SPIRIT LP',
+    tokenDescription: 'test',
+    tokenAddress: '0x30748322B6E34545DBe0788C421886AEB5297789',
+    tokenDecimals: 18,
+    tokenDescriptionUrl: '#',
+    earnedToken: 'eSpiritFTM-SPIRIT',
+    earnedTokenAddress: '0x3d5f5ea4cF0b997702A7Ee849dF6Bb6fe24BB354',
+    earnContractAddress: '0x3d5f5ea4cF0b997702A7Ee849dF6Bb6fe24BB354',
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: 'lps',
+    oracleId: 'spirit-spirit-ftm',
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: 'active',
+    platform: 'SpiritSwap',
+    assets: ['FTM', 'SPIRIT'],
+    callFee: 0.5,
+    addLiquidityUrl: 'https://swap.spiritswap.finance/#/add/',
+    buyTokenUrl: 'https://spiritswap.finance/#/swap',
+  }
+];
